89083ccd0cfd3396e87d2d03cf71429b3bdb3631
zprongua <zprongua@gmail.com>
2023-02-14 15:49:23 -0500

    controllers

--- recipes/src/main/java/cookebook/controller/InstructionController.java
+++ recipes/src/main/java/cookebook/controller/InstructionController.java
@@ -1,7 +1,10 @@
 package cookebook.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import cookebook.repository.InstructionRepository;
 
@@ -15,5 +18,10 @@ public class InstructionController {
     @GetMapping
     private ResponseEntity<Iterable<Instruction>> getInstructions() {
         return new ResponseEntity<>(instructionRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    private ResponseEntity<Instruction> newInstruction(@RequestBody Instruction instruction) {
+        return new ResponseEntity<>(instructionRepository.save(instruction, HttpStatus.CREATED));
     }
 }
