0c2c889b3b4fcad38d2eb3c574a4c13cdda12730
zprongua <zprongua@gmail.com>
2023-02-13 23:58:19 -0500

    contorller work

--- recipes/src/main/java/cookebook/controller/IngredientController.java
+++ recipes/src/main/java/cookebook/controller/IngredientController.java
@@ -1,8 +1,23 @@
 package cookebook.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequestMapping('/ingredient')
 public class IngredientController {
 
+    @Autowired
+    IngredientRepository ingredientRepository;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Ingredient>> getIngredients() {
+        return new ResponseEntity<>(ingredientRepository.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Ingredient> newIngredient(@RequestBody Ingredient ingredient) {
+        return new ResponseEntity<>(ingredientRepository.save(), HttpStatus.CREATED);
+    }
 }
