b727567dc9b09d80e368389117174d516637c186
zprongua <zprongua@gmail.com>
2023-03-06 12:40:37 -0500

    added styling to homepage

--- recipes/src/main/webapp/src/App.tsx
+++ recipes/src/main/webapp/src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-3">
+      <div className="container-fluid rounded p-3 bg-dark text-light">
+        <h1>Welcome to Cook-e-Book</h1>
+        <h4>An online cookbook to help you keep track of recipes and ingredients in your pantry, making cooking a breeze.</h4>
+      </div>
     </div>
   );
 }
