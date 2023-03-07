88d537736cd5415050e9b3365180f7fe2859e99c
zprongua <zprongua@gmail.com>
2023-03-07 09:40:00 -0500

    logos, header banner

--- recipes/src/main/webapp/src/App.tsx
+++ recipes/src/main/webapp/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App p-3">
+      <Header/>
       <div className="container-fluid rounded p-3 bg-dark text-light">
-        <h1>Welcome to Cook-e-Book</h1>
         <h4>An online cookbook to help you keep track of recipes and ingredients in your pantry, making cooking a breeze.</h4>
       </div>
     </div>
---
+++ recipes/src/main/webapp/src/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="text-center bg-light rounded">
+            <img src="../logo.png" alt="Cook-e-Book" />
+        </div>
+    )
+}
+
+export default Header
