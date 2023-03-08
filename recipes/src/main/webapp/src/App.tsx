a38c5ff0979012245896ab2bc38042c46ea178e4
zprongua <zprongua@gmail.com>
2023-03-08 17:30:53 -0500

    color theming

--- recipes/src/main/webapp/src/App.tsx
+++ recipes/src/main/webapp/src/App.tsx
@@ -4,11 +4,11 @@ import Header from './Header';
 
 function App() {
   return (
-    <div className="App p-3">
-      <Header/>
-      <div className="container-fluid rounded p-3 bg-dark text-light">
+    <div className="App container-fluid">
+      <div className="container-fluid p-3 bg-dark text-light bg-gradient">
         <h4>An online cookbook to help you keep track of recipes and ingredients in your pantry, making cooking a breeze.</h4>
       </div>
+      <Header/>
     </div>
   );
 }
--- recipes/src/main/webapp/src/Header.tsx
+++ recipes/src/main/webapp/src/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
     return (
-        <div className="text-center bg-light rounded">
+        <div className="text-center">
             <img src="../logo.png" alt="Cook-e-Book" />
         </div>
     )
