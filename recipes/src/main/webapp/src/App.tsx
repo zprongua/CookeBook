155d1c5528dd1ec91697cd322a6313e392632045
zprongua <zprongua@gmail.com>
2023-03-09 17:36:23 -0500

    new background, working on header

--- recipes/src/main/webapp/src/App.tsx
+++ recipes/src/main/webapp/src/App.tsx
@@ -5,9 +5,6 @@ import Header from './Header';
 function App() {
   return (
     <div className="App container-fluid">
-      <div className="container-fluid p-3 bg-dark text-light bg-gradient">
-        <h4>An online cookbook to help you keep track of recipes and ingredients in your pantry, making cooking a breeze.</h4>
-      </div>
       <Header/>
     </div>
   );
--- recipes/src/main/webapp/src/Header.tsx
+++ recipes/src/main/webapp/src/Header.tsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Header() {
     return (
-        <div className="text-center">
-            <img src="../logo.png" alt="Cook-e-Book" />
+        <div className="logo row">
+            <div className="col-2"></div>
+            <div className="col text-start">
+                <div className="logo row">
+                    <div className="col-4 text-start"><img src="../logo.png" alt="Cook-e-Book" /></div>
+                    <div className="col">
+                        An online cookbook to help you keep track of recipes and ingredients in your pantry, making cooking a breeze.
+                    </div>
+                </div>
+            </div>
+            <div className="col-2"></div>
         </div>
     )
 }
