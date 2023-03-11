dc6344025073478fc805abf0ae5df0f194d0edfb
zprongua <zprongua@gmail.com>
2023-03-11 12:23:00 -0500

    adding sub pages

---
+++ recipes/src/main/webapp/src/Cookbook.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Cookbook() {
+    return (
+        <div>
+            <h1>This is your Cook-e-Book</h1>
+            <ul>
+                <li>Chicken Alfredo</li>
+                <li>Keto Pizza</li>
+                <li>Steak and Taters</li>
+                <li>Peanut Butter Cheesecake</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Cookbook
---
+++ recipes/src/main/webapp/src/Pantry.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Pantry() {
+    return (
+        <div>
+            <h1>This Is Your Pantry</h1>
+        </div>
+    )
+}
+
+export default Pantry
