fed34ac0745a64234ccc46bd291c1624c3a7e0fe
zprongua <zprongua@gmail.com>
2023-03-10 19:01:00 -0500

    color scheme, homepage layout

--- recipes/src/main/webapp/src/App.tsx
+++ recipes/src/main/webapp/src/App.tsx
@@ -6,6 +6,27 @@ function App() {
   return (
     <div className="App container-fluid">
       <Header/>
+      <div className="row">
+        <div className="col-3"></div>
+        <div className="col-5 text-start">
+          Features:
+          <ul>
+            <li>Store your recipes online for quick access wherever you are.</li>
+            <li>Keep track of ingredients in your pantry.</li>
+            <li>Create shopping lists based on your recipes.</li>
+            <li>Sugest recipes based on ingredients you already have.</li>
+          </ul>
+        </div>
+        <div className="col-3"></div>
+      </div>
+      <div className="row">
+        <div className="col-3"></div>
+          <div className="col-5 text-center">
+            <button type="button" className="btn btn-secondary">Get Started</button>
+            <button type="button" className="btn btn-secondary">Login</button>
+          </div>
+        <div className="col-3"></div>
+      </div>
     </div>
   );
 }
