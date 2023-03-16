0802609615415b04629ec34a5eee89742e01fe8f
zprongua <zprongua@gmail.com>
2023-03-15 21:45:44 -0400

    added recipe pages

---
+++ recipes/src/main/webapp/src/Recipe.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Recipe() {
+    return (
+        <div className="text-center">
+            <h2>Cheesecake</h2>
+            <ul>
+                <li>1lb cream cheese</li>
+                <li>1 cup graham crackers</li>
+                <li>1/2 cup sugar</li>
+            </ul>
+            <ul>
+                <li>Preheat oven to 350F</li>
+                <li>Mix ingredients</li>
+                <li>Bake</li>
+                <li>Cool</li>
+                <li>Eat</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Recipe
